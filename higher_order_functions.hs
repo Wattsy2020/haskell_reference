116d2b4ef9d5d1402e1418c48a57a7e901dcb4c4
Liam Watts <liam.watts271@gmail.com>
2023-03-05 02:38:58 +0800

    initial commit

---
+++ higher_order_functions.hs
@@ -0,0 +1,70 @@
+all' :: [Bool] -> Bool
+all' [] = True -- vaccuous truth
+all' [x] = x
+all' (x:xs) = x && all' xs
+-- main = print (all' [True, True], all' [True, True, False])
+
+replicate' :: Integral b => a -> b -> [a]
+replicate' item times
+    | times < 0     = error "cannot replicate a negative amount of times"
+    | times == 0    = []
+    | otherwise     = item : replicate' item (times - 1)
+--main = print (replicate' "hello" 4)
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x, y) : zip' xs ys
+-- main = print (zip' [1, 2, 3] [1, 2])
+
+elem' :: Eq a => a -> [a] -> Bool
+elem' _ [] = False
+elem' item (x:xs) = item == x || elem' item xs
+-- main = print (elem' 1 [3, 1, 2], elem' "Hello" ["There", "Kenobi", "Hello"], elem' 42 [1, 2, 3])
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ _ [] = []
+zipWith' _ [] _ = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+--  main = print (zipWith' (+) [1,2,3] [4, 5, 6])
+
+dotProd :: Num a => [a] -> [a] -> a
+dotProd xs ys = sum (zipWith (*) xs ys)
+-- main = print (dotProd [1, 2, 3] [4, 5, 6])
+
+largest = head (filter (\x -> x `mod` 3829 == 0) [100000, 99999..1])
+-- main = print largest
+
+squareSum = sum (filter odd (takeWhile (<10000) [x^2 | x <- [1..]]))
+-- main = print squareSum
+
+collatzChain :: Int -> [Int]
+collatzChain x
+    | x == 1    = [1]
+    | odd x     = x : collatzChain (3*x + 1)
+    | even x    = x : collatzChain (x `div` 2)
+collatzLength = length . collatzChain
+numLongCollatz = length (filter (>15) (map collatzLength [1..100]))
+--main = print numLongCollatz
+
+-- Implement with folding
+all2 :: [Bool] -> Bool
+all2 = foldl (&&) True -- can also just use the and func
+-- main = print (all2 [True, True], all2 [True, True, False])
+
+elem2 :: Eq a => a -> [a] -> Bool
+elem2 item = foldl (\acc x -> acc || item == x) False
+-- main = print (elem2 1 [3, 1, 2], elem2 "Hello" ["There", "Kenobi", "Hello"], elem2 42 [1, 2, 3], elem2 1 [])
+
+sum' :: Num a => [a] -> a
+sum' = foldr (+) 0
+-- main = print (sum [1, 2, 3, 4])
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x acc -> f x : acc) []
+-- main = print (map' (+1) [1, 2, 3, 4])
+
+-- how many square roots of the natural numbers does it take to get for their sum to be over 1000
+cumulativeSumSqrts = scanl1 (+) (map sqrt [1..])
+answer = length (takeWhile (<1000) cumulativeSumSqrts)
+main = print answer
