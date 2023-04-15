ec5e4fb8d2ffe0cdd498b9b52a7efa8927d2f69a
Liam Watts <liam.watts271@gmail.com>
2023-04-16 04:16:51 +0800

    define default for sum [] = 0

--- reference.hs
+++ reference.hs
@@ -32,7 +32,6 @@ maximum' (x : xs) = max x (maximum' xs)
 
 sum' :: (Num a) => [a] -> a
 sum' [] = 0
-sum' [x] = x
 sum' (x : xs) = x + sum' xs
 
 -- main = print (sum' nums, sum' [1.2, 3.4, 5.6], sum' [])
