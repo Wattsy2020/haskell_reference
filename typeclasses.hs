c28794eb4778f5cdc67e3d8f8e015e7cc79c3483
Liam Watts <liam.watts271@gmail.com>
2023-05-31 22:54:39 +1000

    typeclasses: implement a group over multiplication and addition using CyclicEnum typeclass

--- typeclasses.hs
+++ typeclasses.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE DeriveAnyClass #-}
+
 import Data.Function (on)
 import Data.List qualified as List
 import Data.Set qualified as Set
@@ -29,6 +32,7 @@ allContainer = LambdaContainer (const True)
 toLambda :: (Container f a) => f a -> LambdaContainer a
 toLambda container = LambdaContainer (contains container)
 
+{-
 main = do
   let list = [4 .. 10]
   let set = Set.fromList [1, 2, 3, 4, 5]
@@ -40,3 +44,51 @@ main = do
   let containers = [toLambda list, toLambda set, evenLambda]
   let unionContainer = foldl1 union containers
   print $ filter (contains unionContainer) [1 .. 10]
+-}
+
+class CyclicEnum a where
+  pred' :: a -> a
+  succ' :: a -> a
+
+  default pred' :: (Bounded a, Enum a, Eq a) => a -> a
+  pred' x
+    | x == (minBound :: a) = maxBound :: a
+    | otherwise = pred x
+
+  default succ' :: (Bounded a, Enum a, Eq a) => a -> a
+  succ' x
+    | x == (maxBound :: a) = minBound :: a
+    | otherwise = succ x
+
+data Ring = Zero | One | Two | Three | Four | Five deriving (Show, Eq, Enum, Bounded, CyclicEnum)
+
+instance Num Ring where
+  (+) :: Ring -> Ring -> Ring
+  (+) r1 Zero = r1
+  (+) r1 r2 = succ' r1 + pred r2
+
+  -- the inverse of the number x is y, where x + y = Zero (i.e. the additive identity)
+  negate :: Ring -> Ring
+  negate Zero = Zero
+  negate x = succ $ negate (succ' x)
+
+  (*) :: Ring -> Ring -> Ring
+  (*) r1 Zero = Zero
+  (*) r1 r2 = r1 + (r1 * pred r2)
+
+  abs :: Ring -> Ring
+  abs = id
+
+  signum :: Ring -> Ring
+  signum = const One
+
+  fromInteger :: Integer -> Ring
+  fromInteger 0 = Zero
+  fromInteger x = succ' $ fromInteger (x - 1)
+
+main = do
+  print (succ' Two, succ' Five, pred' Zero)
+  print (One + One, Four + Four, Five + 2)
+  print (negate One, negate Two, negate Five)
+  print (Two - One, Five - Four, Five - Three, One - One)
+  print (Two * Two, 2 * Five)
