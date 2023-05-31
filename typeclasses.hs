3195af5e54664bcf42fb554bd2eaa2ec418ba9b1
Liam Watts <liam.watts271@gmail.com>
2023-05-31 14:58:11 +1000

    typeclasses: finally implement the container typeclass for lambda functions

--- typeclasses.hs
+++ typeclasses.hs
@@ -1,3 +1,4 @@
+import Data.Function (on)
 import Data.List qualified as List
 import Data.Set qualified as Set
 
@@ -14,11 +15,29 @@ instance Container Set.Set where
   contains :: Ord a => Set.Set a -> a -> Bool
   contains = flip Set.member
 
--- todo: figure out how to implement this for all functions that return a bool, e.g. (a -> Bool)
--- for some reason it's hard to write that as a constraint
+newtype LambdaContainer a = LambdaContainer (a -> Bool)
+
+instance Container LambdaContainer where
+  contains :: LambdaContainer a -> a -> Bool
+  contains (LambdaContainer f) = f
+
+union :: (Container f1, Container f2, Ord a) => f1 a -> f2 a -> LambdaContainer a
+union c1 c2 = LambdaContainer (\x -> contains c1 x && contains c2 x)
+
+allContainer :: LambdaContainer a
+allContainer = LambdaContainer (const True)
+
+toLambda :: (Container f, Ord a) => f a -> LambdaContainer a
+toLambda container = LambdaContainer (contains container)
 
 main = do
-  let list = [5 .. 10]
+  let list = [4 .. 10]
   let set = Set.fromList [1, 2, 3, 4, 5]
-  print (contains set 1, contains set 10)
-  print (contains list 1, contains list 10)
+  let evenLambda = LambdaContainer even
+  print (contains set 1, contains set 10, contains evenLambda 4)
+  print (contains list 1, contains list 10, contains evenLambda 5)
+  print $ filter (contains $ union list set) [1 .. 10]
+  print $ filter (contains $ union set evenLambda) [1 .. 10]
+  let containers = [toLambda list, toLambda set, evenLambda]
+  let unionContainer = foldl1 union containers
+  print $ filter (contains unionContainer) [1 .. 10]
