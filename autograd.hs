3b35d93e696c64559091bdfde786c9123b68e667
Liam Watts <liam.watts271@gmail.com>
2023-06-05 15:42:58 +1000

    autograd: refactor to use a function to calculate the derivative

--- autograd.hs
+++ autograd.hs
@@ -14,48 +14,37 @@ instance Show Operator where
   show Signum = "signum"
 
 data Node a
-  = Node {inputNodes :: [Node a], operator :: Operator, gradient :: Node a}
+  = Node {inputNodes :: [Node a], operator :: Operator}
   | Constant {value :: a}
   | Variable {name :: String}
-  | NullDerivative -- to indicate that a derivative node has no gradient (prevent infinitely calculating the gradient recursively)
 
 instance Show a => Show (Node a) where
   show :: Node a -> String
   show (Constant num) = show num
   show (Variable string) = string
-  show (Node inputNodes operator _) =
+  show (Node inputNodes operator) =
     case inputNodes of
       [x1, x2] -> "(" ++ show x1 ++ " " ++ show operator ++ " " ++ show x2 ++ ")"
       [x] -> "(" ++ show operator ++ " " ++ show x ++ ")"
 
-showDerivative :: Show a => Node a -> String
-showDerivative = show . gradient
-
-derivative :: Num a => Node a -> Node a
-derivative (Constant _) = Constant 0
-derivative (Variable _) = Constant 1
-derivative (Node _ _ gradient) = gradient
+showDerivative :: (Show a, Num a) => Node a -> String
+showDerivative = show . derivative
 
 instance Num a => Num (Node a) where
   (+) :: Node a -> Node a -> Node a
-  (+) n1 n2 = Node [n1, n2] Add (Node [derivative n1, derivative n2] Add NullDerivative)
+  (+) n1 n2 = Node [n1, n2] Add
 
   (-) :: Node a -> Node a -> Node a
-  (-) n1 n2 = Node [n1, n2] Subtract (Node [derivative n1, derivative n2] Subtract NullDerivative)
+  (-) n1 n2 = Node [n1, n2] Subtract
 
   (*) :: Node a -> Node a -> Node a
-  (*) n1 n2 = Node [n1, n2] Multiply gradient
-    where
-      vu' = Node [n1, derivative n2] Multiply NullDerivative
-      uv' = Node [derivative n1, n2] Multiply NullDerivative
-      gradient = Node [vu', uv'] Add NullDerivative
+  (*) n1 n2 = Node [n1, n2] Multiply
 
-  -- TODO: implement step functions as operators, so we can define the derivative of abs and signum
   abs :: Node a -> Node a
-  abs n1 = Node [n1] Abs NullDerivative
+  abs n1 = Node [n1] Abs
 
   signum :: Node a -> Node a
-  signum n1 = Node [n1] Signum NullDerivative
+  signum n1 = Node [n1] Signum
 
   fromInteger :: Integer -> Node a
   fromInteger = Constant . fromInteger
@@ -63,7 +52,7 @@ instance Num a => Num (Node a) where
 evalNode :: Num a => Node a -> Map String a -> a
 evalNode (Constant num) _ = num
 evalNode (Variable name) varMap = varMap ! name
-evalNode (Node inputNodes operator _) varMap =
+evalNode (Node inputNodes operator) varMap =
   let args = map (`evalNode` varMap) inputNodes
    in case (args, operator) of
         ([x1, x2], Add) -> x1 + x2
@@ -71,6 +60,18 @@ evalNode (Node inputNodes operator _) varMap =
         ([x1, x2], Multiply) -> x1 * x2
         ([x], Abs) -> abs x
         ([x], Signum) -> signum x
+
+derivative :: Num a => Node a -> Node a
+derivative (Constant _) = Constant 0
+derivative (Variable _) = Constant 1
+derivative (Node inputNodes operator) =
+  case (inputNodes, operator) of
+    ([x1, x2], Add) -> derivative x1 + derivative x2
+    ([x1, x2], Subtract) -> derivative x1 - derivative x2
+    ([x1, x2], Multiply) -> (x1 * derivative x2) + (derivative x1 * x2)
+    -- TODO: implement step functions as an operator, so we can define the derivative of abs and signum
+    ([x], Abs) -> error "Not Implemented"
+    ([x], Signum) -> error "Not Implemented"
 
 evalInputs :: Num a => Node a -> String -> [a] -> [a]
 evalInputs expr varName = map (evalNode expr . Map.singleton varName)
@@ -82,12 +83,12 @@ main = do
   print $ showDerivative expr
   let varMap = Map.singleton "x" 5
   print $ evalNode expr varMap
-  print $ evalNode (gradient expr) varMap
+  print $ evalNode (derivative expr) varMap
 
   -- plot a parabola
   let parabola = var * var
   let xs = [-4 .. 4]
   let ys = evalInputs parabola "x" xs
-  let dydxs = evalInputs (gradient parabola) "x" xs
+  let dydxs = evalInputs (derivative parabola) "x" xs
   print (parabola, showDerivative parabola)
   print $ zip3 xs ys dydxs
