a6574f9f05fe36d8446a7806921f2916c7b22a0b
Liam Watts <liam.watts271@gmail.com>
2024-07-10 23:32:17 +0800

    sized_list: best possible implementation

--- sized_list.hs
+++ sized_list.hs
@@ -6,8 +6,9 @@ data Natural = Zero | Succ Natural
 type One = Succ Zero
 
 type family Add (num1 :: Natural) (num2 :: Natural) where
-    Add 'Zero x = x
-    Add ('Succ x) x2 = 'Succ (Add x x2)
+    Add x1 Zero = x1
+    Add Zero x2 = x2
+    Add (Succ x1) x2 = Succ (Add x1 x2)
 
 data List (size :: Natural) elem where
     Nil :: List 'Zero elem
@@ -17,6 +18,10 @@ instance (Show elem) => Show (List n elem) where
   show :: List n elem -> String
   show Nil = "Nil"
   show (Cons x xs) = show x <> " : " <> show xs
+
+instance Functor (List n) where
+  fmap :: (a -> b) -> List n a -> List n b
+  fmap = mapList
 
 singleton :: elem -> List One elem
 singleton x = Cons x Nil
@@ -29,60 +34,65 @@ lastList (Cons x Nil) = x
 lastList (Cons x xs@(Cons _ _)) = lastList xs
 
 append :: List n elem -> elem -> List (Add n One) elem
-append Nil item = Cons item Nil
+append Nil item = singleton item
 append (Cons x xs) item = Cons x $ append xs item
 
 {-
-reverseList' :: List a b -> List a NonEmpty -> List a NonEmpty
+-- encoding that (Cons x xs) gives a list xs with length N - 1 is tricky
+-- also this isn't recognising that n + m++ == n++ + m
+-- we might instead need to use KnownNat from 
+-- https://hackage.haskell.org/package/base-4.20.0.1/docs/GHC-TypeLits.html 
+reverseList' :: List n elem -> List m elem -> List (Add n m) elem
 reverseList' Nil acc = acc
 reverseList' (Cons x xs) acc = reverseList' xs (Cons x acc)
 
--- can also preserve the type of b
-reverseList :: List a b -> List a b
+reverseList :: List n elem -> List n elem
 reverseList Nil = Nil
 reverseList (Cons x xs) = reverseList' xs (Cons x Nil)
+-}
 
--- reverseList (Cons x xs) = reverseList xs
--- is detected as an error as reverseList xs is not guaranteed to be Cons
-
--- derive functor for list
-foldList :: (acc -> elem -> acc) -> acc -> List elem emptyNess -> acc
+foldList :: (acc -> elem -> acc) -> acc -> List n elem -> acc
 foldList _ acc Nil = acc
 foldList f acc (Cons x xs) = foldList f (f acc x) xs
 
-foldrList :: (elem -> acc -> acc) -> acc -> List elem emptyNess -> acc
+safeFold1 :: (elem -> elem -> elem) -> List (Succ n) elem -> elem
+safeFold1 f (Cons x xs) = foldList f x xs
+
+foldrList :: (elem -> acc -> acc) -> acc -> List n elem -> acc
 foldrList _ acc Nil = acc
 foldrList f acc (Cons x xs) = f x (foldrList f acc xs)
 
-safeFold1 :: (a -> a -> a) -> List a NonEmpty -> a
-safeFold1 f (Cons x xs) = foldList f x xs
-
-mapList :: (a -> c) -> List a b -> List c b
+mapList :: (a -> b) -> List n a -> List n b
 mapList _ Nil = Nil
 mapList f (Cons x xs) = Cons (f x) (mapList f xs)
 
-lengthList :: List a b -> Int
+-- todo: see if we can get the length in constant time using KnowNat
+lengthList :: List n b -> Int
 lengthList = foldList (\acc x -> acc + 1) 0
 
--- Concating is difficult
--- We use the Combine type function to prove that combining empty with NonEmpty list is NonEmpty
-concatList :: List a b -> List a c -> List a (Combine b c)
-concatList Nil Nil = Nil
-concatList Nil xs@(Cons _ _) = xs
-concatList xs@(Cons _ _) Nil = xs
-concatList (Cons x xs) second@(Cons _ _) = Cons x (concatList xs second)
+{-
+Also tricky: doesn't recognise commutation
+concatList :: List n elem -> List m elem -> List (Add n m) elem
+concatList Nil xs = xs
+concatList xs Nil = xs
+concatList (Cons x xs) second = Cons x (concatList xs second)
+-}
 
--- this one also give errors when trying to implement
--- because b could be either Empty or NonEmpty
--- and we don't have enough information at the type level to determine which it is
--- to determine whether its empty or not, we need to encode the full length at the type level
--- unconsList :: List a NonEmpty -> (a, List a b)
--- unconsList (Cons x Nil) = (x, Nil)
--- unconsList (Cons x (Cons _ _)) = (x, Cons x Nil)
+unconsList :: List (Succ n) elem -> (elem, List n elem)
+unconsList (Cons x xs) = (x, xs)
+
+{- type constraint issues as usual
+unsnocList' :: List n elem -> List m elem -> elem -> (List (Add n m) elem, elem)
+unsnocList' Nil acc x = (acc, x)
+unsnocList' (Cons x xs) acc prevx = unsnocList' xs (Cons prevx acc) x
+
+unsnocList :: List (Succ n) elem -> (List n elem, elem)
+unsnocList (Cons x Nil) = (Nil, x)
+unsnocList (Cons x xs) = unsnocList' xs Nil x
+-}
 
 -- todo: implement splitAt N, which splits a list into two lists
 -- one with the first N items and second with the remaining items
--}
 
 main :: IO ()
 main = do
@@ -93,13 +103,13 @@ main = do
   -- print $ headList Nil -- rejected by compiler
   print $ lastList list
   print $ append list 1
-  {-
-  print $ reverseList list
   print $ mapList (+ 2) list
+  print $ fmap (*2) list
   print $ foldList (*) 1 list
   print $ safeFold1 (*) list
-  --  print $ concatList list list
-  print $ lengthList list -}
+  print $ lengthList list
+  print $ unconsList list
+  print $ unconsList list2
   where
     list = Cons 5 $ Cons 2 $ Cons 3 Nil
     list2 = singleton 10
