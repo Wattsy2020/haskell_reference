3d5af2de29947feea052e7b64d423a57859b598a
Liam Watts <liam.watts271@gmail.com>
2024-07-10 22:16:31 +0800

    non_empty_list: improve Combine definition to make type checker more powerful and simplify code

--- non_empty_list.hs
+++ non_empty_list.hs
@@ -11,10 +11,10 @@ data Emptiness = Empty | NonEmpty
 -- type function: a function on all types with Kind of Emptiness, that maps to a type of Kind Emptiness
 -- intended for combining Empty and NonEmpty lists, which results in a non empty List
 type family Combine (emptyStatus1 :: Emptiness) (emptyStatus2 :: Emptiness) where
-  Combine 'Empty 'Empty = 'Empty
-  Combine 'Empty 'NonEmpty = 'NonEmpty
-  Combine 'NonEmpty 'Empty = 'NonEmpty
-  Combine 'NonEmpty 'NonEmpty = 'NonEmpty
+  Combine maybeEmpty 'Empty = maybeEmpty
+  Combine maybeEmpty 'NonEmpty = 'NonEmpty
+  Combine 'Empty maybeEmpty = maybeEmpty
+  Combine 'NonEmpty maybeEmpty = 'NonEmpty
 
 -- the second type y is either Empty or NonEmpty, we can use this to control what types a function accepts
 data List x (emptiness :: Emptiness) where
@@ -49,14 +49,13 @@ append :: List a b -> a -> List a NonEmpty
 append Nil item = Cons item Nil
 append (Cons x xs) item = Cons x $ append xs item
 
-reverseList' :: List a b -> List a NonEmpty -> List a NonEmpty
+reverseList' :: List a b -> List a c -> List a (Combine b c)
 reverseList' Nil acc = acc
 reverseList' (Cons x xs) acc = reverseList' xs (Cons x acc)
 
 -- can also preserve the type of b
 reverseList :: List a b -> List a b
-reverseList Nil = Nil
-reverseList (Cons x xs) = reverseList' xs (Cons x Nil)
+reverseList xs = reverseList' xs Nil 
 
 -- reverseList (Cons x xs) = reverseList xs
 -- is detected as an error as reverseList xs is not guaranteed to be Cons
@@ -83,10 +82,9 @@ lengthList = foldList (\acc x -> acc + 1) 0
 -- Concating is difficult
 -- We use the Combine type function to prove that combining empty with NonEmpty list is NonEmpty
 concatList :: List a b -> List a c -> List a (Combine b c)
-concatList Nil Nil = Nil
-concatList Nil xs@(Cons _ _) = xs
-concatList xs@(Cons _ _) Nil = xs
-concatList (Cons x xs) second@(Cons _ _) = Cons x (concatList xs second)
+concatList Nil xs = xs
+concatList xs Nil = xs
+concatList (Cons x xs) second = Cons x (concatList xs second)
 
 -- this one also give errors when trying to implement
 -- because b could be either Empty or NonEmpty
@@ -106,7 +104,7 @@ main = do
   print $ mapList (+ 2) list
   print $ foldList (*) 1 list
   print $ safeFold1 (*) list
-  --  print $ concatList list list
+  print $ concatList list $ reverseList list
   print $ lengthList list
   where
     list = Cons 5 $ Cons 2 $ Cons 3 Nil
