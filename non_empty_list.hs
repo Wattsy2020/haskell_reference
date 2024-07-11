44aa1bf97636a7a9dc2f361a223a97a53518ab9c
Liam Watts <liam.watts271@gmail.com>
2024-07-12 00:43:38 +0800

    non_empty_list: derive monad because why not

--- non_empty_list.hs
+++ non_empty_list.hs
@@ -16,23 +16,38 @@ type family Combine (emptyStatus1 :: Emptiness) (emptyStatus2 :: Emptiness) wher
   Combine 'Empty maybeEmpty = maybeEmpty
   Combine 'NonEmpty maybeEmpty = 'NonEmpty
 
--- the second type y is either Empty or NonEmpty, we can use this to control what types a function accepts
-data List x (emptiness :: Emptiness) where
-  Nil :: List a 'Empty -- here we use 'Empty as a type
-  Cons :: a -> List a b -> List a 'NonEmpty
+-- the first type is either Empty or NonEmpty, we can use this to control what types a function accepts
+data List (emptiness :: Emptiness) elem where
+  Nil :: List 'Empty elem -- here we use 'Empty as a type
+  Cons :: elem -> List e elem -> List 'NonEmpty elem
 
-instance (Show a) => Show (List a b) where
-  show :: List a b -> String
+instance (Show elem) => Show (List e elem) where
+  show :: List e elem -> String
   show Nil = "Nil"
   show (Cons x xs) = show x <> " : " <> show xs
 
-singleton :: a -> List a NonEmpty
+instance Functor (List e) where
+  fmap :: (elem -> newElem) -> List e elem -> List e newElem
+  fmap = mapList
+
+instance Applicative (List NonEmpty) where
+  pure :: a -> List 'NonEmpty a
+  pure x = Cons x Nil
+
+  (<*>) :: List 'NonEmpty (a -> b) -> List 'NonEmpty a -> List 'NonEmpty b
+  (<*>) funcs list = flattenList $ fmap (`fmap` list) funcs
+
+instance Monad (List NonEmpty) where
+  (>>=) :: List 'NonEmpty a -> (a -> List 'NonEmpty b) -> List 'NonEmpty b
+  (>>=) list f = flattenList $ fmap f list 
+
+singleton :: elem -> List NonEmpty elem
 singleton x = Cons x Nil
 
-safeHead :: List a NonEmpty -> a
+safeHead :: List NonEmpty elem -> elem
 safeHead (Cons x xs) = x
 
-safeLast :: List a NonEmpty -> a
+safeLast :: List NonEmpty elem -> elem
 safeLast (Cons x Nil) = x
 safeLast (Cons x xs@(Cons _ _)) = safeLast xs
 
@@ -45,46 +60,52 @@ safeLast (Cons x xs@(Cons _ _)) = safeLast xs
 -- silly 1 = Cons 1 Nil
 
 -- can however take both and produce a single one
-append :: List a b -> a -> List a NonEmpty
+append :: List e elem -> elem -> List NonEmpty elem
 append Nil item = Cons item Nil
 append (Cons x xs) item = Cons x $ append xs item
 
-reverseList' :: List a b -> List a c -> List a (Combine b c)
+reverseList' :: List e1 elem -> List e2 elem -> List (Combine e1 e2) elem
 reverseList' Nil acc = acc
 reverseList' (Cons x xs) acc = reverseList' xs (Cons x acc)
 
 -- can also preserve the type of b
-reverseList :: List a b -> List a b
+reverseList :: List e elem -> List e elem
 reverseList xs = reverseList' xs Nil 
 
 -- reverseList (Cons x xs) = reverseList xs
 -- is detected as an error as reverseList xs is not guaranteed to be Cons
 
 -- derive functor for list
-foldList :: (acc -> elem -> acc) -> acc -> List elem emptyNess -> acc
+foldList :: (acc -> elem -> acc) -> acc -> List e elem -> acc
 foldList _ acc Nil = acc
 foldList f acc (Cons x xs) = foldList f (f acc x) xs
 
-foldrList :: (elem -> acc -> acc) -> acc -> List elem emptyNess -> acc
+foldrList :: (elem -> acc -> acc) -> acc -> List e elem -> acc
 foldrList _ acc Nil = acc
 foldrList f acc (Cons x xs) = f x (foldrList f acc xs)
 
-safeFold1 :: (a -> a -> a) -> List a NonEmpty -> a
+safeFold1 :: (elem -> elem -> elem) -> List NonEmpty elem -> elem
 safeFold1 f (Cons x xs) = foldList f x xs
 
-mapList :: (a -> c) -> List a b -> List c b
+mapList :: (elem -> newElem) -> List e elem -> List e newElem
 mapList _ Nil = Nil
 mapList f (Cons x xs) = Cons (f x) (mapList f xs)
 
-lengthList :: List a b -> Int
+lengthList :: List e elem -> Int
 lengthList = foldList (\acc x -> acc + 1) 0
 
 -- Concating is difficult
 -- We use the Combine type function to prove that combining empty with NonEmpty list is NonEmpty
-concatList :: List a b -> List a c -> List a (Combine b c)
+concatList :: List e1 elem -> List e2 elem -> List (Combine e1 e2) elem
 concatList Nil xs = xs
 concatList xs Nil = xs
 concatList (Cons x xs) second = Cons x (concatList xs second)
+
+-- Here we somehow need a way to say that the list can contain nonEmpty and Empty Lists
+-- for now this can only work for lists that are all NonEmpty
+flattenList :: List NonEmpty (List NonEmpty elem) -> List NonEmpty elem
+flattenList (Cons list1 Nil) = list1
+flattenList (Cons list1 remaining@(Cons _ _)) = concatList list1 $ flattenList remaining
 
 -- this one also give errors when trying to implement
 -- because b could be either Empty or NonEmpty
@@ -101,11 +122,14 @@ main = do
   print $ safeLast list
   print $ append list 1
   print $ reverseList list
-  print $ mapList (+ 2) list
+  print $ fmap (+ 2) list
   print $ foldList (*) 1 list
   print $ safeFold1 (*) list
   print $ concatList list $ reverseList list
   print $ lengthList list
+  print $ Cons (+1) Nil <*> (pure 1 :: List NonEmpty Int)
+  print $ Cons (+1) (Cons (+2) Nil) <*> list
+  print $ list >>= (\elem -> Cons elem (Cons 0 Nil))
   where
     list = Cons 5 $ Cons 2 $ Cons 3 Nil
     list2 = singleton 10
