8a1231f9c6cb0a5e56236951e190b87de869451c
Liam Watts <liam.watts271@gmail.com>
2024-07-29 02:39:53 +1000

    sokoban: interaction refactor

--- app/cs194/Sokoban/Interaction.hs
+++ app/cs194/Sokoban/Interaction.hs
@@ -21,32 +21,46 @@ readMaybe "Right" = Just Right
 readMaybe _ = Nothing
 
 handleEvent :: Event -> Maze -> Maze
-handleEvent (KeyPress "Esc") = const maze
 handleEvent (KeyPress key) = maybe id movePlayer (readMaybe key)
 handleEvent _ = id
 
 drawWorld :: Maze -> Picture
 drawWorld = scaled 0.2 0.2 . translateBlock (Coordinate (-4) (-4)) . drawMaze
 
+data Interaction state = Interaction state (Event -> state -> state) (state -> Picture)
+
 data StartableGameState a = Starting | Running a
 
-startableActivityOf :: forall world . world -> (Event -> world -> world) -> (world -> Picture) -> IO ()
-startableActivityOf initialStateF handleEventF drawWorldF = activityOf Starting handleEvent' drawWorld'
+startableInteraction :: forall state. Interaction state -> Interaction (StartableGameState state)
+startableInteraction (Interaction initialStateF handleEventF drawWorldF) = 
+    Interaction Starting handleEvent' drawWorld'
     where
-        handleEvent' :: Event -> StartableGameState world -> StartableGameState world
+        handleEvent' :: Event -> StartableGameState state -> StartableGameState state
         handleEvent' (KeyPress " ") Starting = Running initialStateF
         handleEvent' _ Starting = Starting
         handleEvent' event (Running state) = Running $ handleEventF event state
 
-        drawWorld' :: StartableGameState world -> Picture
+        drawWorld' :: StartableGameState state -> Picture
         drawWorld' Starting = startScreen
         drawWorld' (Running world) = drawWorldF world
 
+resettableInteraction :: forall state. Interaction state -> Interaction state
+resettableInteraction (Interaction initialStateF handleEventF drawWorldF) =
+    Interaction initialStateF handleEvent' drawWorldF    
+    where
+        handleEvent' :: Event -> state -> state
+        handleEvent' (KeyPress "Esc") = const initialStateF
+        handleEvent' event = handleEventF event
+
+interactionOf :: Interaction a -> IO ()
+interactionOf (Interaction initialStateF handleEventF drawWorldF) = 
+    activityOf initialStateF handleEventF drawWorldF
+
 -- todo: implement the "Won the game" screen
--- we can use the `data Interaction` concept from the lecture 
--- and define a new function that produces an interaction of a winnable game, 
--- showing a game over screen when a condition is met
 -- then we want gameLoop = resetable ( winnable ( startable ( baseInteraction )))
 
 gameLoop :: IO ()
-gameLoop = startableActivityOf maze handleEvent drawWorld
+gameLoop = interactionOf 
+    $ resettableInteraction 
+    $ startableInteraction 
+    $ Interaction maze handleEvent drawWorld
