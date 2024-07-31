35a4182504d0063ef42c8df7ac8b54feadfd4f25
Liam Watts <liam.watts271@gmail.com>
2024-08-01 01:48:48 +1000

    calculator: handle operator precedence (still with minor errors)

--- app/misc/NaturalCalculator.hs
+++ app/misc/NaturalCalculator.hs
@@ -7,6 +7,10 @@ data Op =
     | Multiply 
     deriving (Show, Eq)
 
+precedence :: Op -> Int
+precedence Plus = 0
+precedence Multiply = 1
+
 data Token a = 
     Digit a
     | Operator Op
@@ -43,9 +47,6 @@ data Expression a =
     | Expression (Expression a) Op (Expression a)
     deriving Show
 
-splitAtFirst :: Eq a => a -> [a] -> ([a], [a])
-splitAtFirst x = fmap (drop 1) . break (x ==)
-
 -- combine all the starting tokens that are digits into a number, also returning remaining tokens
 parseDigits :: Num a => a -> [Token a] -> (a, [Token a])
 parseDigits prev ((Digit value) : remaining) = parseDigits (10 * prev + value) remaining
@@ -53,28 +54,33 @@ parseDigits prev remaining = (prev, remaining)
 
 -- parse an expression, given the expression up until this point
 -- returns the remaining tokens after parsing the expression
-parseExpression' :: (Eq a, Num a) => Maybe (Expression a) -> [Token a] -> (Expression a, [Token a])
-parseExpression' (Just expr) [] = (expr, [])
-parseExpression' Nothing (Digit value : remaining) =
+parseExpression' :: (Eq a, Num a) => Maybe (Expression a) -> Bool -> Bool -> [Token a] -> (Expression a, [Token a])
+parseExpression' (Just expr) _ _ [] = (expr, [])
+parseExpression' Nothing highPrecedence isFirstArg (Digit value : remaining) =
     let (number, remainingTokens) = parseDigits value remaining in
-        parseExpression' (Just $ Value number) remainingTokens
+        if highPrecedence && not isFirstArg -- if highPrecedence then return immediately and let the expression with higher precedence evaluate the remaining tokens
+            then (Value number, remainingTokens)
+            else parseExpression' (Just $ Value number) highPrecedence False remainingTokens
 -- todo: handle operator precedence, if there was an operator with higher precedence previously then we should end the expression here and return to it
-parseExpression' (Just leftExpr) (Operator op : remaining) = 
-    let (rightExpr, remainingTokens) = parseExpression' Nothing remaining in
-        parseExpression' (Just $ Expression leftExpr op rightExpr) remainingTokens
--- parse the expression in the brackets, use the CloseParen to mark the end of the expression
-parseExpression' Nothing (OpenParen : remaining) = parseExpression' Nothing remaining
-parseExpression' (Just expr) (CloseParen : remaining) = (expr, remaining)
+-- need to handle cases like 1 + 3 * 2 along with 3 * 2 + 1
+parseExpression' (Just leftExpr) highPrecedence _ (Operator op : remaining) = 
+    let (rightExpr, remainingTokens) = parseExpression' Nothing (highPrecedence || op == Multiply) False remaining 
+        operatorExpr = Expression leftExpr op rightExpr in
+        if highPrecedence -- if highPrecedence then return immediately and let the expression with higher precedence evaluate the remaining tokens
+            then (operatorExpr, remainingTokens)
+            else parseExpression' (Just operatorExpr) False False remainingTokens
+parseExpression' Nothing highPrecedence _ (OpenParen : remaining) = parseExpression' Nothing highPrecedence True remaining
+parseExpression' (Just expr) _ _ (CloseParen : remaining) = (expr, remaining)
 -- report incorrectly formed expressions
-parseExpression' Nothing (Operator _ : _) = error "no previous expression for the operator"
-parseExpression' (Just _) (Digit _ : _) = error "digit shouldn't have a previous expression"
-parseExpression' (Just _) (OpenParen : _) = error "open parenthesis shouldn't have a previous expression"
-parseExpression' Nothing (CloseParen : _) = error "close parenthesis is not matched with an open parenthesis"
-parseExpression' Nothing [] = error "empty expression"
+parseExpression' Nothing _ _ (Operator _ : _) = error "no previous expression for the operator"
+parseExpression' (Just _) _ _ (Digit _ : _) = error "digit shouldn't have a previous expression"
+parseExpression' (Just _) _ _ (OpenParen : _) = error "open parenthesis shouldn't have a previous expression"
+parseExpression' Nothing _ _ (CloseParen : _) = error "close parenthesis is not matched with an open parenthesis"
+parseExpression' Nothing _ _ [] = error "empty expression"
 -- only operator needs the previous expression
 
 parseExpression :: (Eq a, Num a) => [Token a] -> Expression a
-parseExpression = fst . parseExpression' Nothing
+parseExpression = fst . parseExpression' Nothing False True
 
 -- evaluate an operation on its arguments
 evalOp :: Num a => Op -> a -> a -> a
@@ -90,12 +96,22 @@ evalExpression (Expression left op right) = evalOp op (evalExpression left) (eva
 eval :: (Eq a, Num a) => String -> Maybe a
 eval = fmap (evalExpression . parseExpression) . lexExpression
 
+testCases :: [(String, Int)]
+testCases = [("1+3*2", 7),
+    ("3*2+1", 7),
+    ("1+3*2+1",8),
+    ("11+3*(2+1)+2",22)]
+
+testPasses :: (String, Int) -> Bool
+testPasses (exprStr, result) = eval exprStr == Just result
+
 calculatorMain :: IO ()
 calculatorMain = do
+    print $ "Tests: " ++ (if all testPasses testCases then "passed" else "failed")
     print $ "Expression: " ++ expression
     print lexed
     print (fmap parseExpression lexed)
     print (eval expression :: Maybe Int)
     where
-        expression = "11+3*(2+1)+2" -- (11+3*(2+1)+2)+100
+        expression = "(11+3*(2+1+1)+2)+100" -- ("(11+3*(2+1+1)+2)+100",125) currently doesn't work
         lexed = lexExpression expression :: Maybe [Token Int]
